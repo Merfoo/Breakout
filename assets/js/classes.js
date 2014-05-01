99168893412871cfcff76a4acbd669fbed5f8a2c
Merfoo <fauzimerfoo@gmail.com>
2014-04-30 18:48:57 -0700

    Rewrote majority of code.

---
+++ assets/js/classes.js
@@ -0,0 +1,34 @@
+Brick = function()
+{
+    this.x = 0;
+    this.y = 0;
+    this.color = "red";
+    this.startLives = 0;
+    this.lives = 0;
+};
+
+Paddle = function()
+{
+    this.x = 0;
+    this.y = 0;
+    this.startWidth = 175;
+    this.startHeight = 13;
+    this.width = 175;
+    this.height = 13;
+    this.startMaxV = 1;
+    this.startVInc = 0.1;
+    this.maxV = 10;
+    this.v = 0;
+    this.vInc = 0.07;
+    this.vDec = 0.05;
+};
+
+Ball = function()
+{
+    this.x = 10;
+    this.y = 10;
+    this.xV = 7;
+    this.yV = 7;
+    this.r = 10;
+    this.color = "red";
+};
